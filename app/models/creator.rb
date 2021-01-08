964c79ff8eb1a730d395972ac65814126b3af5ef
ganzinn <ganzinnpg@gmail.com>
2021-01-08 15:49:44 +0900

    アソシエーション（アクティブハッシュ）追加

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,4 +1,7 @@
 class Creator < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+
   has_many :families
   has_many :users, through: :families
 end

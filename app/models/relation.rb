d32ad635014863c92a8cd436a070da38f6a78452
ganzinn <ganzinnpg@gmail.com>
2021-01-08 15:48:04 +0900

    アクティブハッシュ追加

---
+++ app/models/relation.rb
@@ -0,0 +1,16 @@
+class Relation < ActiveHash::Base
+  self.data = [
+    { id: 0, value: '---' },
+    { id: 1, value: 'パパ' },
+    { id: 2, value: 'ママ' },
+    { id: 3, value: 'おじいちゃん' },
+    { id: 4, value: 'おばあちゃん' },
+    { id: 5, value: 'おじさん' },
+    { id: 6, value: 'おばさん' },
+    { id: 7, value: '子ども自身' },
+    { id: 8, value: '親族ほか' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :families
+end

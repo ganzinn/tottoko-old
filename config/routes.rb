87bb386efb31be9f7f25b0b0d78eabfcd15f57e4
ganzinn <ganzinnpg@gmail.com>
2021-01-10 04:00:06 +0900

    ルーティング追加（new, create）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: :show
   root to: 'works#index'
-  resources :creators
+  resources :creators do
+    resources :families, only: [:new, :create]
+  end
 
 end

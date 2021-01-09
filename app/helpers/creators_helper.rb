d533b5477190618d26d910eff0eb306f802cc43e
ganzinn <ganzinnpg@gmail.com>
2021-01-09 18:29:47 +0900

    年齢表示ヘルパー作成

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,2 +1,12 @@
 module CreatorsHelper
+  def age(creator)
+    dob = creator.date_of_birth
+    today = Date.today
+
+    age_years = (today.strftime('%Y%m%d').to_i - dob.strftime('%Y%m%d').to_i)/10000.to_i
+    age_months = (today.strftime('%m%d').to_i - dob.strftime('%m%d').to_i)/100.to_i
+    age_months = age_months < 0 ? age_months + 12 : age_months
+
+    return "#{age_years}歳#{age_months}ヶ月"
+  end
 end

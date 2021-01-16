1d5e17efdca7e18b0c76f10c099fa516d8f26306
ganzinn <ganzinnpg@gmail.com>
2021-01-16 17:36:24 +0900

    作品閲覧権限チェック追加

--- app/controllers/user_works_controller.rb
+++ app/controllers/user_works_controller.rb
@@ -1,15 +1,37 @@
 class UserWorksController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @user = User.find(current_user.id)
-    # binding.pry
+    # 閲覧可能の子どもを絞り込み
     user_creator_ids = Family.where(user_id: current_user.id).pluck(:creator_id)
-    @user_creators = Creator.where(id: user_creator_ids)
+    # クエリパラメータで特定の子どもに絞り込んでいる場合のチェック
     if params[:creator_id]
-      select_creator_ids =  params[:creator_id]
+      if user_creator_ids.include?(params[:creator_id].to_i)
+          select_creator_ids =  params[:creator_id]
+      else
+          redirect_to user_works_path
+      end
     else
       select_creator_ids = user_creator_ids
     end
-    @user_works = Work.where(creator_id: select_creator_ids).includes(:creator)
+
+    @user_creators = Creator.where(id: user_creator_ids)
+    @user = User.find(current_user.id)
+
+    # 閲覧権限チェック
+    user_work_ids = []
+    Work.joins("INNER JOIN families ON works.creator_id = families.creator_id")
+        .select("works.id AS id, families.id AS family_id, works.scope_id AS scope_id, families.relation_id AS relation_id")
+        .where(creator_id: select_creator_ids, families: {user_id: current_user})
+        .find_each do |work|
+          if work.scope_id == 3 # 一般公開
+            user_work_ids << work.id
+          else
+            user_work_ids << work.id if work.scope.targets.include?(work.relation_id)
+          end
+        end
+        
+    @user_works = Work.where(id: user_work_ids).order("date DESC").includes(:creator)
   end
 
   def new

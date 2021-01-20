137c93a6c116bdc02eb11c9f09a3e2dd7e27ae75
ganzinn <ganzinnpg@gmail.com>
2021-01-20 23:12:39 +0900

    ヘルパーメソッド追加

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -3,4 +3,14 @@ module WorksHelper
   def edit_permission_check(family)
     return Family.edit_permission_check(family)
   end
+
+  # 公開範囲表示アイコン
+  def work_scope_icon(work)
+    if work.scope.id == 3
+      icon_string = "lock_open"
+    else
+      icon_string = "lock"
+    end
+    return raw("<i class='tiny material-icons'>#{icon_string}</i>")
+  end
 end

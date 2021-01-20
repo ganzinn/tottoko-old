686b66699909705a51e32a1369bb41f994a32662
ganzinn <ganzinnpg@gmail.com>
2021-01-20 23:14:18 +0900

    誕生日計算機能汎用化

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,10 +1,9 @@
 module CreatorsHelper
-  def age(creator)
+  def age(creator, start_day = Date.today)
     dob = creator.date_of_birth
-    today = Date.today
 
-    age_years = (today.strftime('%Y%m%d').to_i - dob.strftime('%Y%m%d').to_i)/10000.to_i
-    age_months = (today.strftime('%m%d').to_i - dob.strftime('%m%d').to_i)/100.to_i
+    age_years = (start_day.strftime('%Y%m%d').to_i - dob.strftime('%Y%m%d').to_i)/10000.to_i
+    age_months = (start_day.strftime('%m%d').to_i - dob.strftime('%m%d').to_i)/100.to_i
     age_months = age_months < 0 ? age_months + 12 : age_months
 
     return "#{age_years}歳#{age_months}ヶ月"
